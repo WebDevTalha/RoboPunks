74fd26dd2b6662cdbcfa2465e9bdc4b245267628
{Talha} <{mdfarhantanvirtalha@gmail.com}>
2023-07-16 21:50:03 +0600

    RoboPunkNFT init git

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.4",
+    networks: {
+        sepolia: {
+            url: process.env.SEPOLIA_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_KEY,
+    },
+}
